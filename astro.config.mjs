ae33ef242a7fdf4b8e905cf14e0325e6549f0b23
Zeshunzue <derikautsar470@gmail.com>
2026-09-15 17:12:57 +0700

    Fix formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zeshunzue.github.io",
+site: "https://zeshunzue.github.io",
   base: "/portfolio",
   vite: {
     plugins: [tailwindcss()],
